4c6e96455f3a4d1b3e569f46ec267bc19ee2bc2d
Nadiar AS <codxse@gmail.com>
2017-02-20 23:38:51 +0700

    .

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ebta-ui "0.1.0-SNAPSHOT"
+  :description "Ebtanas UI"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.473"]
+                 [org.omcljs/om "1.0.0-alpha47"]
+                 [datascript "0.15.5"]
+                 [figwheel-sidecar "0.5.9" :scope "test"]])
---
+++ src/ebta_ui/core.cljs
@@ -0,0 +1,8 @@
+(ns ebta-ui.core
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]))
+
+(enable-console-print!)
+
+(println "Hello world!")
